4ba7c79627ded5fc75297a5bc66b054ffd58b05e
Vladimir Chaykin <9166542201@mail.ru>
2023-01-08 20:51:51 +0300

    third commit

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-document.getElementsByName("input")[0].onclick = f1
 const f = function () {
     const x = document.getElementById("out")
-    if (!x.textContent) x.textContent = "HiHi"
+    if (!x.textContent) x.textContent = "tagAttr"
     else x.textContent = ""
 }
-function f1() {
+const f1 = function () {
     const x = document.getElementById("out")
-    if (!x.textContent) x.textContent = "Hello"
+    if (!x.textContent) x.textContent = "elementProperty"
     else x.textContent = ""
 }
+const f2 = function () {
+    const x = document.getElementById("out")
+    if (!x.textContent) x.textContent = "addEvent"
+    else x.textContent = ""
+}
+document.getElementsByName("input")[0].onclick = f1
+document.querySelector("#event").addEventListener("click", f2)
